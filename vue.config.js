3717374dd671fdcfc64e6ac10b70c9cc3aaf0e3d
thomas <thomchevalier49@gmail.com>
2024-03-28 15:41:27 +0100

    petite folie

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+// vue.config.js
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: [
-    'vuetify'
-  ]
-})
+  transpileDependencies: ['vuetify'],
+  chainWebpack: config => {
+    // Here you can modify the existing HtmlWebpackPlugin instance without adding a new one
+    config.plugin('html').tap(args => {
+      args[0].title = 'Your New Title'; // Example: Changing the title
+      // Add more modifications as needed
+      return args;
+    });
+  },
+});
